d6ece758b9951eab5c4d664d5ca4cbe9c5e88723
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2021-11-30 23:29:25 -0600

    a

---
+++ Ferdev/src/main/java/com/ferdev/FerdevApplication.java
@@ -0,0 +1,13 @@
+package com.ferdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerdevApplication.class, args);
+	}
+
+}
---
+++ Ferdev/src/test/java/com/ferdev/FerdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
